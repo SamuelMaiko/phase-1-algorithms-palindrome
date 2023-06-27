d5745c221c5b00871c0daa09d1ed6d45559ba53c
Samuel Maiko <sammymaiko721@gmail.com>
2023-06-28 01:22:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedWord="";
+  for (let letter of word){
+    reversedWord=letter+reversedWord;
+  }
+    if(reversedWord===word){
+      return true;
+    }
+    else {
+      return false;
+    }
 }
-
+console.log(isPalindrome("magma"));
 /* 
   Add your pseudocode here
+
+  create function called palindrome with parameter word
+  declare a let variable with empty string  
+  use for of to loop through the string
+  add each letter behind  the empty string as you loop
+  use if statement to compare variable value and parameter
+  return the value
 */
 
 /*
   Add written explanation of your solution here
+
+  we loop through the string using the for of loop because strings behave as arrays of letters 
+  so we take each letter and add it behind the empty string stored inside variable it will loop through and 
+  create a new string that is a reversed version of the initial string
+  compare the two using if statement and return true if they are the same and false otherwise 
+
 */
 
 // You can run `node index.js` to view these console logs
